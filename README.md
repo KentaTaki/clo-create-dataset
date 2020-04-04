2e77e4181e787e17b85e915274c8e3096d1df361
DaichiMorimoto <fb0120earth@gmail.com>
2020-04-04 14:12:30 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# CLO の設定
+デスクトップにアプリがあり、アカウント発行済み＋ログインできる状態であるとする。
+1. zprj/sample_hogehoge.ZPrj を一つ開く
+1. CLOの設定 > ユーザ設定 > ショートカット >  3D衣装 >  全パターン再配置に ctrl+Rを登録
+1. CLOの設定 > ユーザ設定 > ショートカット >  アバター > アバターの編集に ctrl+A を登録
+1. CLOのMacのデスクトップ1に画面いっぱいに配置 ( ctrl + 1 でDesktop1に遷移するか確認 )
+1. 右上のObject Browser の→を連打してグレーディングを選択する。
+1. 右下の3Dというマークをクリックする。
+1. 4~6を行うと下図のような図になる。
+1. command+shift+S して、適当な名前で保存する（保存した先がデータセットが溜まっていく場所 your_dir になる）
+
+
+![desktop1](images/desktop1.png)
+
+# 補足
+もしも服に線が入っていたら、左上の服のアイコンをクリックして青色に光っているアイコンを下図のようにしてください。
+![desktop-icon](images/desktop-icon.png)
+
+# Python の設定
+```
+pip install pyautogui==0.9.48
+python automator_CLO.py --dir your_dir --mm --clothtest --sizedataset test.csv #グレーディングの確認
+python automator_CLO.py --dir your_dir --mm --sizedataset test.csv #速度の確認
+python automator_CLO.py --dir your_dir --mm --sizedataset test.csv --pr 1.5 #速度を1.5倍遅くする
+python automator_CLO.py --dir your_dir --mm # 本番
+```
---
+++ automator_CLO.py
@@ -0,0 +1,177 @@
+import os
+import csv
+import time
+import subprocess
+import pyautogui as pg
+
+prescroll = 0
+sizegroup = ["XS", "S", "M", "L", "XL", "XXL"]
+positions = {
+                "avatar": {
+                    "tall": { "x": 678, "y": 268, "scroll": 0 },
+                    "chest": { "x": 690, "y": 295, "scroll": 0 },
+                    "under": { "x": 575, "y": 727, "scroll": 1 },
+                    "waist": { "x": 575, "y": 689, "scroll": 2 },
+                    "hip": { "x": 905, "y": 433, "scroll": 1 },
+                    "yukitake": { "x": 905, "y": 644, "scroll": 1 },
+                    "kata": { "x": 575, "y": 499, "scroll": 1 },
+                    "mata": { "x": 905, "y": 503, "scroll": 1 },
+                    "momo": { "x": 905, "y": 522, "scroll": 1 },
+                    },
+                "grading": {
+                    "button": { "x": 1307, "y": 116 },
+                    "right": { "x": 1433, "y": 116 },
+                    },
+                "cloth": {
+                    "XS": { "x": 1283, "y": 204 },
+                    "S": { "x": 1283, "y": 227 },
+                    "M": { "x": 1283, "y": 246 },
+                    "L": { "x": 1283, "y": 271 },
+                    "XL": { "x": 1283, "y": 296 },
+                    "XXL": { "x": 1283, "y": 316 },
+                    },
+                "scroll": [
+                    { "x": 1070, "y": 389},
+                    { "x": 1070, "y": 721},
+                    ],
+                "save": { "x": 652, "y": 160}
+            }
+
+def init():
+    print('Start GUI Automation...')
+    pg.click(pause=1.0)
+    pg.hotkey('ctrl', '1', pause=1.0) # Move on to Desktop1
+    pg.click(pause=1.0)
+    pg.hotkey('2', pause=1.0)
+
+def open_avatar_size():
+    print("Edit Size")
+    pg.hotkey('ctrl', 'a', pause=1.0)
+    time.sleep(10)
+
+def edit_avatar_size(key, value, pr):
+    global prescroll
+    values = positions["avatar"][key]
+    if values["scroll"] > 0 and values["scroll"] != prescroll:
+        scroll = positions["scroll"][values["scroll"]-1]
+        pg.click(
+            x=scroll["x"],
+            y=scroll["y"],
+            pause=6.0*pr,
+            clicks=1,
+            interval=0,
+            button='left'
+        )
+    prescroll = values["scroll"]
+    pg.click(
+        x=values["x"],
+        y=values["y"],
+        pause=6.0*pr,
+        clicks=1,
+        interval=0,
+        button='left'
+    )
+    pg.hotkey('command', 'a', pause=1.0*pr)
+    pg.typewrite(value, interval=0.2, pause=1.0*pr)
+    pg.hotkey('enter', pause=3.0*pr)
+
+
+def close_avatar_size():
+    print("Closed")
+    pg.hotkey('esc', pause=1.0)
+
+
+def choose_cloth_size(cloth_size):
+    print("Choose "+cloth_size)
+    pg.click(
+        x=positions["cloth"][cloth_size]["x"],
+        y=positions["cloth"][cloth_size]["y"],
+        pause=1.0,
+        clicks=1,
+        interval=0,
+        button='left'
+    )
+    time.sleep(5)
+
+
+def simulation():
+    print('Start Simulation...')
+    pg.hotkey('ctrl', 'r', pause=1.0)
+    pg.hotkey('space', pause=1.0)
+    time.sleep(10)
+    pg.hotkey('space', pause=1.0)
+    time.sleep(3)
+
+
+def save_zprj(basename, directory, pr, is_torso=False):
+    print("Save file")
+    if is_torso:
+        pg.hotkey('shift','a', pause=1.0*pr)
+        filepath = basename+'-torso'
+    else:
+        filepath = basename
+    # save
+    pg.hotkey('shift','command','s', pause=4.0*pr)
+    pg.click(
+        x=positions["save"]["x"],
+        y=positions["save"]["y"],
+        pause=5.0*pr,
+        clicks=4,
+        interval=0,
+        button='left'
+    )
+    pg.hotkey('delete', pause=2.0*pr)
+    pg.typewrite(filepath, interval=0.2, pause=2.0*pr)
+    pg.hotkey('enter', pause=2.0*pr)
+    print("File Saved " + filepath)
+
+    time.sleep(5*pr)
+    if is_torso:
+        pg.hotkey('shift', 'a', pause=1.0*pr)
+    # Remove zprj file
+    command = "rm " + os.path.abspath(directory) + os.sep + filepath +".Zprj"
+    subprocess.run(command, shell=True, check=True)
+
+
+def get_size_dataset(csv_path, is_mm):
+    with open(csv_path) as f:
+        reader = csv.DictReader(f)
+        l = []
+        for row in reader:
+            if is_mm:
+                func = lambda x: (x[0], float(x[1])*10.0)
+            else:
+                func = lambda x: (x[0], float(x[1]))
+            l.append(list(map(func, row.items())))
+        return l
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--dir', type=str, help='directory')
+    parser.add_argument('--pr', default=1.0, type=float, help='pause rate')
+    parser.add_argument('--name', default='', type=str, help='data name')
+    parser.add_argument('--sizedataset', default='sample.csv', type=str, help='size chart (csv)')
+    parser.add_argument('--mm', action='store_true', help='mm mode')
+    parser.add_argument('--clothtest', action='store_true', help='mm mode')
+    args = parser.parse_args()
+    size_dataset = get_size_dataset(args.sizedataset, args.mm)
+    if args.clothtest:
+        name = 'test'
+    else:
+        name = args.name
+
+    # GUI Automation
+    init()
+    for i, size_data in enumerate(size_dataset):
+        # Edit Avatar Size
+        if not args.clothtest:
+            open_avatar_size()
+            for key, val in size_data:
+                edit_avatar_size(key, str(val), args.pr)
+            close_avatar_size()
+        # Edit Cloth Size
+        for cloth_size in sizegroup:
+            choose_cloth_size(cloth_size) # choose
+            simulation() # Draping
+            save_zprj(name+str(i).zfill(3)+'-'+cloth_size, args.dir, args.pr, is_torso=False) #Save File
